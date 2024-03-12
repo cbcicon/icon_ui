6465704eb1641f7c5f57b96befd4d46a958072e8
Rakesh <rakeshbaghel9621@gmail.com>
2024-03-12 14:02:59 +0530

    file structuring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppLayoutComponent } from './layout/app.layout.component';
+import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  
+    {
+        path: '', component: AppLayoutComponent,
+        children: [
+            { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+            { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
+            { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
+        ]
+    },
+  
+    { path: 'notfound', component: NotfoundComponent },
+    { path: '**', redirectTo: '/notfound' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/layout/app.menu.component.ts
@@ -0,0 +1,102 @@
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LayoutService } from './service/app.layout.service';
+
+@Component({
+    selector: 'app-menu',
+    templateUrl: './app.menu.component.html'
+})
+export class AppMenuComponent implements OnInit {
+
+    model: any[] = [];
+
+    constructor(public layoutService: LayoutService) { }
+
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Main',
+                items: [
+                    { label: 'Dashboard', icon: 'dashboard-icon', routerLink: ['/'] },
+                    { label: 'Forecast', icon: 'forecaste', routerLink: ['/'] },
+                    
+                ]
+            },
+            {
+                label: 'Material Resource Planning',
+                items: [
+                    
+                    {
+                        label: 'Orders',
+                        icon: 'receipt-icon',
+                        routerLink: ['/pages/empty']
+                    },
+                    {
+                        label: 'Inventory',
+                        icon: 'inventory-icon',
+                        items: [
+                            {
+                                label: 'Login',
+                                icon: 'pi pi-fw pi-sign-in',
+                                routerLink: ['/auth/login']
+                            },
+                            {
+                                label: 'Error',
+                                icon: 'pi pi-fw pi-times-circle',
+                                routerLink: ['/auth/error']
+                            },
+                            {
+                                label: 'Access Denied',
+                                icon: 'pi pi-fw pi-lock',
+                                routerLink: ['/auth/access']
+                            }
+                        ]
+                    },
+                    {
+                        label: 'Procure',
+                        icon: 'handbag-icon',
+                        routerLink: ['/pages/empty']
+                    },
+                    {
+                        label: 'Make',
+                        icon: 'stopwatch-icon',
+                        routerLink: ['/pages/empty']
+                    },
+                    {
+                        label: 'Manufacturers',
+                        icon: 'people-icon',
+                        routerLink: ['/pages/empty']
+                    },
+                    {
+                        label: 'Capacity',
+                        icon: 'battery-half-icon',
+                        routerLink: ['/pages/empty']
+                    },
+                    {
+                        label: 'What If?',
+                        icon: 'question-circle-icon',
+                        routerLink: ['/pages/empty']
+                    },
+                    {
+                        label: 'Reports',
+                        icon: 'bar-chart-icon',
+                        routerLink: ['/pages/empty']
+                    },
+                ]
+            } ,
+            {
+                label: 'Others',
+                items: [
+                    { label: 'Suppliers', icon: 'box-seam-icon', routerLink: ['/'] },
+                    { label: 'Logistics', icon: 'truck-icon', routerLink: ['/'] },
+                ]
+            },
+            {
+                items: [
+                    { label: 'Settings', icon: 'setting-icon', routerLink: ['/'] },
+                    { label: 'Sign Out', icon: 'signout-icon', routerLink: ['/'] },
+                ]
+            },
+        ];
+    }
+}
