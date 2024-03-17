51ade63679d6507c04778b6e75d587f21b2708f5
Hemadri Peram <hemadri.p@cloudbclabs.com>
2024-03-18 01:22:49 +0530

    Api Integration

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,18 +1,45 @@
 import { Component, OnInit} from '@angular/core';
-
-
+import { ApiService } from './dashboard.service';
+import { TableModule } from 'primeng/table';
 
 @Component({
-    templateUrl: './dashboard.component.html',
+selector: 'app-root',
+  template: `
+    <p-table [value]="Users" [tableStyle]="{ 'min-width': '50rem' }">
+    <ng-template pTemplate="header">
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>User Id</th>
+            <th>Email</th>
+        </tr>
+    </ng-template>
+    <ng-template pTemplate="body" let-u>
+        <tr>
+            <td>{{ u.firstname }}</td>
+            <td>{{ u.lastname }}</td>
+            <td>{{ u.userid }}</td>
+            <td>{{ u.emailid }}</td>
+        </tr>
+    </ng-template>
+</p-table>
+  `,
 })
 export class DashboardComponent implements OnInit {
-
-    constructor() {
+    public Users: any = [];  
+    constructor(private apiService: ApiService) {
       
     }
 
     ngOnInit() {
        
     }
-
+    
+    ngAfterViewInit(): void {  
+        this.apiService.getUsers().subscribe((data: any[]) => {
+            this.Users = data;
+          });
+      
+     
+        }
 }
---
+++ src/app/components/dashboard/dashboard.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  apiUrl = 'http://localhost:5021/api';
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/User`);
+  }
+
+  getUserById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/User/${id}`);
+  }
+
+}
