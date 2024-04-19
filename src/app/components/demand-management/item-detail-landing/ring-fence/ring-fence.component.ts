3b3cf23ba9d8f467e3ac9fd31e68a48ddc3dbe36
sateesh barla <sateesh.b@cloudbclabs.com>
2024-04-20 02:18:13 +0530

    adding down  componet

---
+++ src/app/components/demand-management/item-detail-landing/ring-fence/ring-fence.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ring-fence',
+  templateUrl: './ring-fence.component.html',
+  styleUrls:['./ring-fence.component.scss']
+})
+export class RingFenceComponent {}
