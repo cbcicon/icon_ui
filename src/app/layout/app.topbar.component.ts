94839d3d9507be794524dd8f523c55552cb362c7
Sateesh Barla <sateesh.b@cloudbclabs.com>
2024-05-09 22:06:29 +0530

    chat box

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { LayoutService } from "./service/app.layout.service";
+import { Sidebar } from 'primeng/sidebar';
+
 
 @Component({
     selector: 'app-topbar',
@@ -10,17 +12,41 @@ export class AppTopBarComponent {
 
     items!: MenuItem[];
 
+    alertRightSideBar :boolean = false;
+
+    unreadCommentLength = 0;
+
+    allComments = []
+
     @ViewChild('menubutton') menuButton!: ElementRef;
 
     @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
 
     @ViewChild('topbarmenu') menu!: ElementRef;
+    @ViewChild('sidebarRef') sidebarRef!: Sidebar;
+
+    
 
     constructor(public layoutService: LayoutService) { }
 
+    ngOnInit(){ }
+
     get getComments(): boolean {
+  
         let chatData = JSON.parse(localStorage.getItem('comments') || '[]');
-      
+
+        this.allComments = chatData;
+
+        let unreadCount = 0;
+
+      chatData.forEach((comment:any) => {
+        if (!comment.read) {
+          unreadCount++;
+        }
+      });
+
+      this.unreadCommentLength = unreadCount
+
         if (chatData.length > 0) {
           return chatData.some((comment:any) => !comment.read);
         } else {
@@ -28,15 +54,24 @@ export class AppTopBarComponent {
         }
       }
 
+      closeCallback(e:any): void {
+        this.sidebarRef.close(e);
+     }
+    
 
       handleReadComments(){
         const existingComments =JSON.parse(localStorage.getItem('comments') || '[]');
-
         const updatedComments = existingComments.map((comment:any )=> {
             comment.read = true;
           return comment;
         });
         localStorage.setItem('comments', JSON.stringify(updatedComments));
       }
+
+
+
+ hideShowAlertSidebar(){
+       this.alertRightSideBar = ! this.alertRightSideBar
+     }
 
 }
