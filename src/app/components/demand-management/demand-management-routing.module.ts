cf2ff5015e82534ec67b2dd495fa24304a813ed6
Sateesh Barla <sateesh.b@cloudbclabs.com>
2024-05-29 08:43:36 +0530

    phase-1

--- src/app/components/demand-management/demand-management-routing.module.ts
+++ src/app/components/demand-management/demand-management-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ForecastTable } from './forecast/forecast-table';
 import { NewstudyTableComponent } from './newstudy/newstudy-table.component';
+import { MasterDemandPlanComponent } from './master-demand-management/master-demand-plan';
 
 const routes: Routes = [
+  { path: 'master-demand-plan', component: MasterDemandPlanComponent },
   { path: 'forecast', component: ForecastTable },
   {path:'new-study', component: NewstudyTableComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule] 
 })
 export class DemandManagementRoutingModule { }
+ 
