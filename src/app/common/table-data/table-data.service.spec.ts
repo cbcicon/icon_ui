742afa2b963048de8767cfd47cfe211a11ea23ff
Sateesh Barla <sateesh.b@cloudbclabs.com>
2024-06-07 19:21:03 +0530

    new ui changes and api addition

---
+++ src/app/common/table-data/table-data.service.spec.ts
@@ -0,0 +1,9 @@
+import { TableDataService } from './table-data.service';
+
+describe('TableDataService', () => {
+  const service: TableDataService = new TableDataService();
+
+  it('should create', () => {
+    expect(service).toBeTruthy();
+  });
+});
