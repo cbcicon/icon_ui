71dddf9ee1cbbe00f4b3e351a8cf7aafaaf48b63
Sateesh Barla <sateesh.b@cloudbclabs.com>
2024-07-05 08:48:08 +0530

    Inventory config Component

--- src/app/components/inventory-management/inventory-configuration/inv-config-purchase-order/inv-config-purchase-order.ts
+++ src/app/components/inventory-management/inventory-configuration/inv-config-purchase-order/inv-config-purchase-order.ts
@@ -5,4 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './inv-config-purchase-order.html',
   styleUrls:[ './inv-config-purchase-order.scss']
 })
-export class InvConfigPurchaseOrderComponent {}
+export class InvConfigPurchaseOrderComponent {
+
+  selectedValue:any;
+
+  monthList = [{ value: '1', name: '1 month' },
+  { value: '2', name: '2 month' },
+  { value: '3', name: '3 month' },
+  { value: '4', name: '4 month' },
+  { value: '5', name: '5 month' },
+  ]
+
+
+  onSponsorChange(event: any) {
+
+    let searchParam = event.value.value;
+
+    console.log("test : " ,searchParam)
+
+  }
+
+}
--- src/app/components/inventory-management/inventory-configuration/inv-config-safety-stock/inv-config-safety-stock.ts
+++ src/app/components/inventory-management/inventory-configuration/inv-config-safety-stock/inv-config-safety-stock.ts
@@ -7,5 +7,104 @@ import { Component } from '@angular/core';
 })
 export class InvConfigSafetyStockComponent {
   
+
+  selectedValue:any;
+
+  loading:boolean = false;
+  totalRecords:number = 0 ;
+
+  invConfigSafetyStockData:any =[
+    {
+      actions: "",
+      location: "Buxton",
+      configType: "Location",
+      minStock: 100,
+      maxStock: 500,
+    
+      createdDate: "01 Jan 2024",
+      lastModifiedDate: "15 Jan 2024",
+    },
+    {
+      actions: "",
+      location: "Farmingdale",
+      configType: "Location",
+      minStock: 3.0,
+      maxStock: 600,
+    
+      createdDate: "01 Jan 2024",
+      lastModifiedDate: "18 Jan 2024",
+    },
+    {
+      actions: "",
+      location: "Therepak CZ",
+      configType: "Location",
+      minStock: 50,
+      maxStock: 300,
+    
+      createdDate: "01 Jan 2024",
+      lastModifiedDate: "23 Jan 2024",
+    },
+    {
+      actions: "",
+      location: "Midland",
+      configType: "Location",
+      minStock: 100,
+      maxStock: 400,
+    
+      createdDate: "01 Jan 2024",
+      lastModifiedDate: "7 Feb 2024",
+    },
+    {
+      actions: "",
+      location: "Tennant EU",
+      configType: "Location",
+      minStock: 350,
+      maxStock: 400,
+    
+      createdDate: "01 Jan 2024",
+      lastModifiedDate: "10 Feb 2024",
+    },
+    {
+      actions: "",
+      location: "Shannon EU",
+      configType: "Location",
+      minStock: 300,
+      maxStock: 250,
+     
+      createdDate: "01 Jan 2024",
+      lastModifiedDate: "19 Feb 2024",
+    },
+    {
+      actions: "",
+      location: "Global",
+      configType: "Location",
+      minStock: 250,
+      maxStock: 850,
+    
+      createdDate: "01 Jan 2024",
+      lastModifiedDate: "22 Feb 2024",
+    }
+  ]
+  
+  
+
+
+  monthList = [{ value: '1', name: '1 month' },
+  { value: '2', name: '2 month' },
+  { value: '3', name: '3 month' },
+  { value: '4', name: '4 month' },
+  { value: '5', name: '5 month' },
+  ]
+
+
+  onSponsorChange(event: any) {
+
+    let searchParam = event.value.value;
+
+    console.log("test : " ,searchParam)
+
+  }
+
+
 }
 
