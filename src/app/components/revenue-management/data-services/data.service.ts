e4dcc52281feaf4ca341f0a33ebd2d55494befc4
DevlinaBhatnagarCloudBClabs <devlina.b@cloudbclabs.com>
2024-08-06 10:24:13 +0530

    revenue json data sub-components and grid

--- src/app/components/revenue-management/data-services/data.service.ts
+++ src/app/components/revenue-management/data-services/data.service.ts
@@ -93,4 +93,111 @@ export class DataService {
     // Return the data as an Observable
     return of(data);
   }
+
+  getProductionData() {
+    return [
+      {
+        "Sponsor": this.sanitizer.bypassSecurityTrustHtml("<a href='#' style='color: rgba(18, 132, 116, 1); text-decoration: underline;'>Duke</a>"),
+        "Protocol": "PD-001",
+        "Cohort": "Standard",
+        "Revenue": "$1000",
+        "Margin": "15%"
+      },
+      {
+        "Sponsor": this.sanitizer.bypassSecurityTrustHtml("<a href='#' style='color: rgba(18, 132, 116, 1); text-decoration: underline;'>Duke</a>"),
+        "Protocol": "PD-002",
+        "Cohort": "US",
+        "Revenue": "$3000",
+        "Margin": "10%"
+      },
+      {
+        "Sponsor": this.sanitizer.bypassSecurityTrustHtml("<a href='#' style='color: rgba(18, 132, 116, 1); text-decoration: underline;'>Duke</a>"),
+        "Protocol": "PD-003",
+        "Cohort": "Japan",
+        "Revenue": "$5000",
+        "Margin": "18%"
+      },
+      {
+        "Sponsor": this.sanitizer.bypassSecurityTrustHtml("<a href='#' style='color: rgba(18, 132, 116, 1); text-decoration: underline;'>Molecular</a>"),
+        "Protocol": "PD-004",
+        "Cohort": "Standard",
+        "Revenue": "$5000",
+        "Margin": "13%"
+      },
+      {
+        "Sponsor": this.sanitizer.bypassSecurityTrustHtml("<a href='#' style='color: rgba(18, 132, 116, 1); text-decoration: underline;'>Molecular</a>"),
+        "Protocol": "PD-005",
+        "Cohort": "Asia",
+        "Revenue": "$8000",
+        "Margin": "5%"
+      },
+      {
+        "Sponsor": this.sanitizer.bypassSecurityTrustHtml("<a href='#' style='color: rgba(18, 132, 116, 1); text-decoration: underline;'>Molecular</a>"),
+        "Protocol": "PD-006",
+        "Cohort": "Europe",
+        "Revenue": "$400",
+        "Margin": "-10%"
+      }
+    ];
+  }
+
+  getSponsorData() {
+    return [
+      {
+        "Protocol": "DP-001",
+        "Cohort": "Standard",
+        "Visit": "V1",
+        "KitName": "Hematology",
+        "KitQty": "23",
+        "PreLabel":"No",
+        "KitComplexityLevel":"Low"
+      },
+      {
+        "Protocol": "DP-001",
+        "Cohort": "Standard",
+        "Visit": "V2",
+        "KitName": "Tissue",
+        "KitQty": "64",
+        "PreLabel":"Yes",
+        "KitComplexityLevel":"High"
+      },
+      {
+        "Protocol": "DP-001",
+        "Cohort": "Standard",
+        "Visit": "V8",
+        "KitName": "Unscheduled",
+        "KitQty": "74",
+        "PreLabel":"Yes",
+        "KitComplexityLevel":"Medium"
+      },
+      {
+        "Protocol": "DP-002",
+        "Cohort": "US",
+        "Visit": "V5",
+        "KitName": "Hematology",
+        "KitQty": "48",
+        "PreLabel":"No",
+        "KitComplexityLevel":"Low"
+      },
+      {
+        "Protocol": "DP-002",
+        "Cohort": "US",
+        "Visit": "V6",
+        "KitName": "Hematology",
+        "KitQty": "101",
+        "PreLabel":"Yes",
+        "KitComplexityLevel":"High"
+      },
+      {
+        "Protocol": "DP-002",
+        "Cohort": "US",
+        "Visit": "V7",
+        "KitName": "Tissue",
+        "KitQty": "38",
+        "PreLabel":"No",
+        "KitComplexityLevel":"Medium"
+      }
+    ];
+  }
+
 }
