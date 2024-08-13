a40b24f0d6dea178cea0e6939c46906f551aa2b0
Sateesh Barla <sateesh.b@cloudbclabs.com>
2024-08-14 00:37:33 +0530

    tabel header

--- src/app/components/search-page/search-page.module.ts
+++ src/app/components/search-page/search-page.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { SearchPageRoutingModule } from './search-page-routing.module';
 import { SearchPageTablesComponent } from './search-page-tables/search-page-tables.component';
+import { ImportsModule } from '../../import';
 
 
 @NgModule({
   declarations: [SearchPageTablesComponent],
   imports: [
     CommonModule,
+    ImportsModule,
     SearchPageRoutingModule
   ]
 })
